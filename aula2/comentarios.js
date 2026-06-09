15b275ea62db432e7b5ec5008a4f6edac3bc2442
hfdec <hf.dev54@gmail.com>
2026-06-09 14:08:26 -0300

    feat: Adiciona conteudos sobre a aula 2

---
+++ aula2/comentarios.js
@@ -0,0 +1,14 @@
+// Implementando comentário, 01 
+console.log("Hello World");
+console.log("Hello USA");
+// comentário 02
+console.log("Hello Australia");
+console.log("Hello UK");
+
+/* 
+    Comentario em bloco
+    lorem 
+    lorem 
+    lorem
+    tudo inserido dentro desse bloco sera ignorado
+*/
