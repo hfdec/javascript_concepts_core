6c8238619021ca02042f9f9a3f27901ae0b57d27
hfdec <hf.dev54@gmail.com>
2026-06-12 17:38:12 -0300

    feat: Adiciona conteúdo de tipos primitivos em js

---
+++ aula6/index.js
@@ -0,0 +1,10 @@
+const nome = 'Luiz'; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num1 = 10; // number 
+const num2 = 10.52; // number 
+let nomeAluno; // Undefined -> nao aponta pra local nenhuma na memória 
+const sobrenomeAluno = null; // nulo, diferente do undefined
+const aprovado = false; // boolean - true or false 
+
+console.log(typeof num1, aprovado) // exibindo o tipo deles
