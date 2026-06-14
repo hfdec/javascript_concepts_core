ff2d1bcf05ac0a6b34394cef94b0a080362bf985
hfdec <hf.dev54@gmail.com>
2026-06-14 11:53:00 -0300

    feat: Adiciona conteudo sobre conversao de numeros, operadores aritmeticos

---
+++ aula07/index.js
@@ -0,0 +1,9 @@
+// Incremento = ++ 
+// Decremento = --
+// Nan -  Not a number
+// ParseInt(conversao do numero para um inteiro), parseFloat(decimais)
+let num2 = parseFloat(5.2);
+const num1 = 5;
+console.log(num2);
+console.log(num1);
+
